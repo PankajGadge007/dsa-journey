a5c354c39a44e798d34c9f7217e3c947d4c8d251
Pankaj <pankaj.g@intelegain.com>
2026-03-08 10:50:46 +0530

    Simple Conditioning update

--- dsa/src/com/kotlin/logic_building/phase_2/test.kt
+++ dsa/src/com/kotlin/logic_building/phase_2/test.kt
@@ -1,4 +1,12 @@
 package com.kotlin.logic_building.phase_2
 
-class test {
+fun main() {
+    val test = Test()
+    test.printMsg()
+}
+
+class Test {
+    fun printMsg() {
+        println("testing")
+    }
 }
